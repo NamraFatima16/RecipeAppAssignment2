56288ade0487a501a7d1f2eee2844d479ac70e3c
namra <20094326@mail.wit.ie>
2023-05-09 10:24:48 +0530

    Added image uri to recipeModel

--- app/src/main/java/ie/setu/recipeapp/models/RecipeModel.kt
+++ app/src/main/java/ie/setu/recipeapp/models/RecipeModel.kt
@@ -1,5 +1,6 @@
 package ie.setu.recipeapp.models
 
+import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.util.*
@@ -10,4 +11,5 @@ data class RecipeModel(var title: String = "",
                        var servings: Int = 0,
                        var timeRequired: String = "",
                        var instructions: String = "",
+                       var image: Uri = Uri.EMPTY,
                        val id: UUID = UUID.randomUUID()) : Parcelable
