0d46efcc58a1749cf7c288fd0207196dda53807f
namra <20094326@mail.wit.ie>
2023-05-10 16:28:13 +0530

    Implemented in-memory store

---
+++ app/src/main/java/ie/setu/recipeapp/models/store/RecipeMemStore.kt
@@ -0,0 +1,38 @@
+package ie.setu.recipeapp.models.store
+
+import ie.setu.recipeapp.models.RecipeModel
+import timber.log.Timber.i
+import java.util.UUID
+
+class RecipeMemStore : RecipeStore {
+
+    val recipes = ArrayList<RecipeModel>()
+
+    override fun create(recipe: RecipeModel) {
+        i("Adding recipe: $recipe")
+        recipes.add(recipe)
+    }
+
+    override fun read(): List<RecipeModel> {
+        return recipes
+    }
+
+    override fun update(recipe: RecipeModel) {
+        var foundRecipe = findById(recipe.id)
+        i("Updating recipe: $foundRecipe")
+        foundRecipe?.let {
+            foundRecipe = recipe.copy()
+        }
+        i("Updated recipe: $foundRecipe")
+    }
+
+    override fun delete(recipe: RecipeModel) {
+        i("Removing recipe $recipe")
+        recipes.remove(recipe)
+    }
+
+    override fun findById(id: UUID): RecipeModel? {
+        return recipes.find { it.id == id }
+    }
+
+}
--- app/src/main/java/ie/setu/recipeapp/models/store/RecipeStore.kt
+++ app/src/main/java/ie/setu/recipeapp/models/store/RecipeStore.kt
@@ -1,11 +1,12 @@
 package ie.setu.recipeapp.models.store
 
 import ie.setu.recipeapp.models.RecipeModel
+import java.util.UUID
 
 interface RecipeStore {
     fun create(recipe: RecipeModel)
     fun read(): List<RecipeModel>
     fun update(recipe: RecipeModel)
     fun delete(recipe: RecipeModel)
-    fun findById(id:Long) : RecipeModel?
+    fun findById(id:UUID) : RecipeModel?
 }
